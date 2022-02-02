dca15a93949c863fc60455721157b26d39496631
HEMZ <HEMZ@DESKTOP-U8T55F0>
2022-02-02 14:38:40 +0530

    SECOND COMMIT

--- StudentMgmtSystem/src/main/java/com/spring/StudentMgmtSystemApplication.java
+++ StudentMgmtSystem/src/main/java/com/spring/StudentMgmtSystemApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudentMgmtSystemApplication {
-
+//
 	public static void main(String[] args) {
 		SpringApplication.run(StudentMgmtSystemApplication.class, args);
 	}
